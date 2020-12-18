e102ad6e2d75074ea89d384ecafe739e509369c4
truizdiaz <tomiruizdiaz@gmail.com>
2020-12-18 15:29:52 -0300

    Resultado Component - Mostramos resultado

--- src/app/components/resultado/resultado.component.ts
+++ src/app/components/resultado/resultado.component.ts
@@ -7,14 +7,32 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./resultado.component.css']
 })
 export class ResultadoComponent implements OnInit {
-  bmi: number
+  bmi: number;
+  resultado: string;
+  interpretacion: string;
 
   constructor(private route: ActivatedRoute) { 
+    this.resultado = '';
+    this.interpretacion = '';
     this.bmi = +route.snapshot.paramMap.get('valor')!;
-    console.log(this.bmi)
   }
 
   ngOnInit(): void {
+    this.getResultado();
+  }
+
+  getResultado() {
+
+    if(this.bmi >=25) {
+      this.resultado = 'Exceso de peso';
+      this.interpretacion = 'Tienes un peso corporal superior al normal. Intente hacer más ejercicio'
+    } else if(this.bmi >= 18.5) {
+      this.resultado = 'Normal';
+      this.interpretacion = 'Tienes un peso corporal normal. ¡Buen trabajo!'
+    } else {
+      this.resultado = 'Bajo peso';
+      this.interpretacion = 'Tienes un peso corporal más bajo de lo normal. Puedes comer un poco más'
+    }
   }
 
 }
