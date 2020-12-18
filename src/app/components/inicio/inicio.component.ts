f96ab5e03af31526c3a5372f2c5312438b8c3a3e
truizdiaz <tomiruizdiaz@gmail.com>
2020-12-18 10:50:32 -0300

    Altura dinamico

--- src/app/components/inicio/inicio.component.ts
+++ src/app/components/inicio/inicio.component.ts
@@ -8,10 +8,15 @@ import { Component, OnInit } from '@angular/core';
 export class InicioComponent implements OnInit {
   edad = 25;
   peso = 60;
+  altura = 170;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  cambiarAltura(event: any) {
+    this.altura = event.target.value;
   }
 
 }
