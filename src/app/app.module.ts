8718dac934400df012255e00d4a3184cf84d64ae
truizdiaz <tomiruizdiaz@gmail.com>
2020-12-18 09:09:01 -0300

    Creacion de componentes - agregamos bootstra-google fonts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { ResultadoComponent } from './components/resultado/resultado.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    ResultadoComponent
   ],
   imports: [
     BrowserModule,
